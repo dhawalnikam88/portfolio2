c7c6f7bb2cc278fb0af8773eedc35e954cad6d3c
dhawalnikam88 <dhawalnikam88@gmail.com>
2020-12-21 03:43:19 +0000

    Deploying to gh-pages from  @ a36d52dc659ba42f2c13dfd3d9e290fdce071184 🚀

---
+++ precache-manifest.38a58b635df4e9be8804ffc648fb7456.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2225b7f4c63574fa80abc298b81e57e4",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "456ebcd3f231b94be58f",
+    "url": "/portfolio2/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "8339a3c65476406ea4d3",
+    "url": "/portfolio2/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "bb2bc98d197f937bc536",
+    "url": "/portfolio2/static/css/main.d0a3791f.chunk.css"
+  },
+  {
+    "revision": "c8043514d9e1d975bbcd",
+    "url": "/portfolio2/static/js/2.cc7f3afa.chunk.js"
+  },
+  {
+    "revision": "456ebcd3f231b94be58f",
+    "url": "/portfolio2/static/js/3.7239d54f.chunk.js"
+  },
+  {
+    "revision": "8339a3c65476406ea4d3",
+    "url": "/portfolio2/static/js/4.0e902777.chunk.js"
+  },
+  {
+    "revision": "bb2bc98d197f937bc536",
+    "url": "/portfolio2/static/js/main.fcd40767.chunk.js"
+  },
+  {
+    "revision": "b61fa7729f61ba90c1e7",
+    "url": "/portfolio2/static/js/runtime-main.489c063c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio2/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio2/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/portfolio2/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio2/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio2/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio2/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio2/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/portfolio2/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/portfolio2/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio2/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/portfolio2/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/portfolio2/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "ff9b567182d63846d0899c3280357e85",
+    "url": "/portfolio2/static/media/kmrgroupLogo.ff9b5671.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio2/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio2/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "f32b3f892b2ff3a1fbd1d7cead2d120b",
+    "url": "/portfolio2/static/media/microsoftLogo.f32b3f89.svg"
+  },
+  {
+    "revision": "45fb5330e1880463befe4f62c18f7e55",
+    "url": "/portfolio2/static/media/missouriLogo.45fb5330.png"
+  },
+  {
+    "revision": "2a6f5744d76eb4d8bc807a1359d8db19",
+    "url": "/portfolio2/static/media/missouriLogo2.2a6f5744.jpg"
+  },
+  {
+    "revision": "c57f4434d33c053a7a8fa96de038931c",
+    "url": "/portfolio2/static/media/missouriSeal.c57f4434.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio2/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "b6290fb340676542e8d15546e4acc116",
+    "url": "/portfolio2/static/media/profilePic.b6290fb3.jpg"
+  },
+  {
+    "revision": "1d407e3c232cb5d5f8d1dfc7fc2d63b5",
+    "url": "/portfolio2/static/media/profilePic2.1d407e3c.jpg"
+  },
+  {
+    "revision": "186dd58b678cf14a3b4813b1c8a1a951",
+    "url": "/portfolio2/static/media/profilePic3.186dd58b.jpg"
+  },
+  {
+    "revision": "5034b7962ada2b3e276ae2ce80e34962",
+    "url": "/portfolio2/static/media/profilePic4.5034b796.jpg"
+  },
+  {
+    "revision": "b1c440e63875dbfdd7b10e58a6d15bac",
+    "url": "/portfolio2/static/media/profilePic5.b1c440e6.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfolio2/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "e609415db07633bd582929f69c4e55d7",
+    "url": "/portfolio2/static/media/puneUniversityLogo.e609415d.jpg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/portfolio2/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio2/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/portfolio2/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio2/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio2/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/portfolio2/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio2/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/portfolio2/static/media/talksCardBackAlt.b48e86c9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developerFolio/precache-manifest.3e24eefe1a3b57695d85b36b02e89e54.js"
+  "/portfolio2/precache-manifest.38a58b635df4e9be8804ffc648fb7456.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
