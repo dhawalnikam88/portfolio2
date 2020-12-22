18399db27c6ffb6a243cdf95d895a6a0005c917a
Dhawal Nikam <dhawalnikam88@gmail.com>
2020-12-21 21:32:40 -0800

    Default to light theme

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -29,8 +29,8 @@ export default class Main extends Component {
   }
 
   componentDidMount() {
-    const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-    this.setState({ isDark: darkPref.matches });
+    const darkPref = window.matchMedia("(prefers-color-scheme: light)");
+    this.setState({ isDark: false }); // darkPref.matches
   }
   changeTheme = () => {
     this.setState({ isDark: !this.state.isDark });
